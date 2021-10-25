01029f9d6deec6907bbcd54555b9d50115e1f770
Jaisael <jaisael@protonmail.com>
2021-10-25 11:02:27 +0200

    Added database and tcp support.
    Database supported is sqlite.  Tcp listens on default telnet port 23.

--- main.go
+++ main.go
@@ -1,6 +1,45 @@
 package main
 
-import ()
+import (
+	"bufio"
+	"database/sql"
+	"log"
+	"net"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
+	//Connection related data is stored in a sqlite database.
+	db, err := sql.Open("sqlite3", "./foo.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	db.Ping()
+
+	//Listen on port 23 - the default telnet port.
+	ln, err := net.Listen("tcp", ":23")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ln.Close()
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Print("Couldn't accept connection.'")
+			return
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(c net.Conn) error {
+	for {
+		msg, err := bufio.NewReader(c).ReadBytes('\n')
+		if err != nil {
+			return err
+		}
+		c.Write(msg)
+	}
 }
