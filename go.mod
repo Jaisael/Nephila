2352dc1dc830d794d5cf2a5e360e9121ca645777
Jaisael <jaisael@protonmail.com>
2021-10-26 22:06:54 +0200

    Added the skeleton of a login dialogue.
    Passwords are hashed using the bcrypt package,

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/Jaisael/Nephila
 go 1.17
 
 require github.com/mattn/go-sqlite3 v1.14.9
+
+require golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"database/sql"
 	"log"
 	"net"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -33,14 +36,76 @@ func main() {
 	}
 }
 
+type Connection struct {
+	con           net.Conn
+	username      string
+	passwordwrong int
+	state         int
+}
+
 func handleConnection(c net.Conn) error {
+	newConn := &Connection{con: c, state: 1}
 	c.Write([]byte("Welcome."))
+	c.Write([]byte("Username:"))
 	reader := bufio.NewReader(c)
 	for {
-		msg, err := reader.ReadBytes('\n')
+		msg, err := reader.ReadString('\n')
 		if err != nil {
 			return err
 		}
-		log.Print(msg)
+		switch newConn.state {
+		case 1:
+			if msg != "" {
+				if characterExists(msg) {
+					newConn.state = 2
+					newConn.username = strings.Title(strings.ToLower(msg))
+					c.Write([]byte("Password:"))
+				} else {
+					c.Write([]byte("No record exists of a character with this name. Would you like to create a character with this name?"))
+					newConn.state = 3
+				}
+
+			} else {
+				c.Write([]byte("Username:"))
+			}
+		case 2:
+			if checkPassword(msg, "") {
+				newConn.state = 4
+			} else {
+				newConn.passwordwrong++
+				if newConn.passwordwrong > 2 {
+					c.Write([]byte("That is not this character's password. Please try again later."))
+					c.Close()
+				} else {
+					c.Write([]byte("That is not the password for this character."))
+					c.Write([]byte("What is the password for this character?"))
+				}
+			}
+		case 3:
+
+		default:
+			log.Print(msg)
+		}
 	}
+}
+
+func hashPassword(p string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+	}
+	return string(hash)
+}
+
+func checkPassword(p, h string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(h), []byte(p))
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	return true
+}
+
+func characterExists(s string) bool {
+	return true
 }
